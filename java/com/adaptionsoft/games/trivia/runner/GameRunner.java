fbf4f21ea880a245d65cf01a934dabacc4524b51
toni.jara <toni.jara@gmail.com>
2012-04-20 08:52:17 +0200

    [TEST] Incluidos tests andamio con cambios mínimos de código. [REFACTOR] Inyección de dados y objeto Game para tests andamio.

--- java/com/adaptionsoft/games/trivia/runner/GameRunner.java
+++ java/com/adaptionsoft/games/trivia/runner/GameRunner.java
@@ -1,36 +1,51 @@
-
 package com.adaptionsoft.games.trivia.runner;
+
 import java.util.Random;
 
 import com.adaptionsoft.games.uglytrivia.Game;
-
 
 public class GameRunner {
 
 	private static boolean notAWinner;
 
+	private static Random rand = new Random();
+	private static Random randRoll = new Random();
+
+	private static Game aGame = new Game();
+
+	public static Game getaGame() {
+		return aGame;
+	}
+
+	public static void setaGame(Game game) {
+		aGame = game;
+	}
+
+	public void setRand(Random rand) {
+		this.rand = rand;
+	}
+
+	public void setRandRoll(Random randRoll) {
+		this.randRoll = randRoll;
+	}
+
 	public static void main(String[] args) {
-		Game aGame = new Game();
-		
+
 		aGame.add("Chet");
 		aGame.add("Pat");
 		aGame.add("Sue");
-		
-		Random rand = new Random();
-	
+
 		do {
-			
-			aGame.roll(rand.nextInt(5) + 1);
-			
+
+			aGame.roll(randRoll.nextInt(5) + 1);
+
 			if (rand.nextInt(9) == 7) {
 				notAWinner = aGame.wrongAnswer();
 			} else {
 				notAWinner = aGame.wasCorrectlyAnswered();
 			}
-			
-			
-			
+
 		} while (notAWinner);
-		
+
 	}
 }
---
+++ java/com/adaptionsoft/games/trivia/runner/GameRunnerTest.java
@@ -0,0 +1,91 @@
+package com.adaptionsoft.games.trivia.runner;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import com.adaptionsoft.games.uglytrivia.Game;
+
+public class GameRunnerTest {
+	
+	SpyPrintStream outputStream;
+	GameRunner runner;
+	Game game;
+	
+	@Before
+	public void setUp() {
+		outputStream = new SpyPrintStream();
+		System.setOut(outputStream);
+
+		runner = new GameRunner();
+		game = new Game();
+		runner.setaGame(game);
+	}
+
+	@Test
+	public void launchGameWithRandomOneAndChetWinsWithSixCoinsAndLocationSix() {
+		
+		Random mockRandom = Mockito.mock(Random.class);
+		Mockito.when(mockRandom.nextInt()).thenReturn(1);
+		runner.setRand(mockRandom);
+		runner.setRandRoll(mockRandom);
+		
+		runner.main(null);
+		
+		Assert.assertEquals(3,runner.getaGame().howManyPlayers());
+		Assert.assertTrue(runner.getaGame().isPlayable());
+		
+		Assert.assertTrue(outputStream.contains("Chet's new location is 6"));
+		Assert.assertTrue(outputStream.contains("Sports Question 3"));
+		Assert.assertTrue(outputStream.contains("Chet now has 6 Gold Coins."));
+	}
+	
+	@Test
+	public void launchGameWithRollAlwaysTwoAndRandomAnswerOne() {
+		
+		Random mockRandom = Mockito.mock(Random.class);
+		Mockito.when(mockRandom.nextInt()).thenReturn(1);
+		Random mockRandomRoll = Mockito.mock(Random.class);
+		Mockito.when(mockRandomRoll.nextInt()).thenReturn(2);
+		runner.setRand(mockRandom);
+		runner.setRandRoll(mockRandomRoll);
+		
+		runner.main(null);
+		
+		Assert.assertEquals(3,runner.getaGame().howManyPlayers());
+		Assert.assertTrue(runner.getaGame().isPlayable());
+		
+		Assert.assertTrue(outputStream.contains("Chet's new location is 6"));
+		Assert.assertTrue(outputStream.contains("Sports Question 3"));
+		Assert.assertTrue(outputStream.contains("Chet now has 6 Gold Coins."));
+	}	
+
+}
+
+class SpyPrintStream extends PrintStream {
+	List<String> buffer = new ArrayList<String>();
+	
+	public SpyPrintStream() {
+		super(Mockito.mock(OutputStream.class));
+	}
+	
+	public void println(String str) {
+		buffer.add(str);
+	}
+	
+	public void println(Object obj) {
+		buffer.add(obj.toString());
+	}
+	
+	public boolean contains(String str) {
+		return buffer.contains(str);
+	}
+}
